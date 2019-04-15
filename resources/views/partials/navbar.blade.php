669d95059a0a5ebde8bb5da825dac23f6e4bb523
Essa <essa.khan.pro@gmail.com>
2019-04-15 19:42:16 +0100

    1)Started to sort out views 2)Added non-functional navigation bar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,33 @@
+
+
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a class="navbar-brand" href="/"> <i class="fas fa-paw"></i> AstonAdoptAnimal <i class="fas fa-paw"></i></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="/services">Services</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="fas fa-user"></i> User Account</a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown04">
+                              <a class="dropdown-item" href="#"> User Account</a>
+                              <a class="dropdown-item" href="#">Another action</a>
+                              <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+
+       
+        
